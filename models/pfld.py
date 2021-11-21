954c36fa41972c052f23d682058a44563938e2aa
Junyan Li <lijunyan668@outlook.com>
2021-11-21 09:54:32 +0800

    ori

--- models/pfld.py
+++ models/pfld.py
@@ -63,121 +63,121 @@ class InvertedResidual(nn.Module):
 
 
 
-class PFLDInference(nn.Module):
-    
-    def x1_hook(self, module, input, output):
-        self.x1 = output
-    
-    def x2_hook(self, module, input, output):
-        self.x2 = output
-    
-    def out1_hook(self, module, input, output):
-        self.out1 = output
-        
-    def __init__(self):
-        super(PFLDInference, self).__init__()
-        self.backbone = _make_backbone()
-        
-        self.backbone.blocks[4].register_forward_hook(self.out1_hook)
-        self.backbone.blocks[8].register_forward_hook(self.x1_hook)
-        self.backbone.blocks[16].register_forward_hook(self.x2_hook)
-        
-        self.avg_pool1 = nn.AvgPool2d(14)
-        self.avg_pool2 = nn.AvgPool2d(7)
-        self.fc = nn.Linear(432, 196)
-
-    def forward(self, x):  # x: 3, 112, 112
-        x3 = self.backbone(x)
-        x1 = self.avg_pool1(self.x1)
-        x2 = self.avg_pool2(self.x2)
-        
-        x1 = x1.view(x1.size(0), -1)
-        x2 = x2.view(x2.size(0), -1)
-        x3 = x3.view(x3.size(0), -1)
-        
-        multi_scale = torch.cat([x1, x2, x3], 1)
-        landmarks = self.fc(multi_scale)
-        return self.out1, landmarks
-
 # class PFLDInference(nn.Module):
+    
+#     def x1_hook(self, module, input, output):
+#         self.x1 = output
+    
+#     def x2_hook(self, module, input, output):
+#         self.x2 = output
+    
+#     def out1_hook(self, module, input, output):
+#         self.out1 = output
+        
 #     def __init__(self):
 #         super(PFLDInference, self).__init__()
-
-#         self.conv1 = nn.Conv2d(3,
-#                                64,
-#                                kernel_size=3,
-#                                stride=2,
-#                                padding=1,
-#                                bias=False)
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.relu = nn.ReLU(inplace=True)
-
-#         self.conv2 = nn.Conv2d(64,
-#                                64,
-#                                kernel_size=3,
-#                                stride=1,
-#                                padding=1,
-#                                bias=False)
-#         self.bn2 = nn.BatchNorm2d(64)
-#         self.relu = nn.ReLU(inplace=True)
-
-#         self.conv3_1 = InvertedResidual(64, 64, 2, False, 2)
-
-#         self.block3_2 = InvertedResidual(64, 64, 1, True, 2)
-#         self.block3_3 = InvertedResidual(64, 64, 1, True, 2)
-#         self.block3_4 = InvertedResidual(64, 64, 1, True, 2)
-#         self.block3_5 = InvertedResidual(64, 64, 1, True, 2)
-
-#         self.conv4_1 = InvertedResidual(64, 128, 2, False, 2)
-
-#         self.conv5_1 = InvertedResidual(128, 128, 1, False, 4)
-#         self.block5_2 = InvertedResidual(128, 128, 1, True, 4)
-#         self.block5_3 = InvertedResidual(128, 128, 1, True, 4)
-#         self.block5_4 = InvertedResidual(128, 128, 1, True, 4)
-#         self.block5_5 = InvertedResidual(128, 128, 1, True, 4)
-#         self.block5_6 = InvertedResidual(128, 128, 1, True, 4)
-
-#         self.conv6_1 = InvertedResidual(128, 16, 1, False, 2)  # [16, 14, 14]
-
-#         self.conv7 = conv_bn(16, 32, 3, 2)  # [32, 7, 7]
-#         self.conv8 = nn.Conv2d(32, 128, 7, 1, 0)  # [128, 1, 1]
-#         self.bn8 = nn.BatchNorm2d(128)
-
+#         self.backbone = _make_backbone()
+        
+#         self.backbone.blocks[4].register_forward_hook(self.out1_hook)
+#         self.backbone.blocks[8].register_forward_hook(self.x1_hook)
+#         self.backbone.blocks[16].register_forward_hook(self.x2_hook)
+        
 #         self.avg_pool1 = nn.AvgPool2d(14)
 #         self.avg_pool2 = nn.AvgPool2d(7)
-#         self.fc = nn.Linear(176, 196)
+#         self.fc = nn.Linear(432, 196)
 
 #     def forward(self, x):  # x: 3, 112, 112
-#         x = self.relu(self.bn1(self.conv1(x)))  # [64, 56, 56]
-#         x = self.relu(self.bn2(self.conv2(x)))  # [64, 56, 56]
-#         x = self.conv3_1(x)
-#         x = self.block3_2(x)
-#         x = self.block3_3(x)
-#         x = self.block3_4(x)
-#         out1 = self.block3_5(x)
-
-#         x = self.conv4_1(out1)
-#         x = self.conv5_1(x)
-#         x = self.block5_2(x)
-#         x = self.block5_3(x)
-#         x = self.block5_4(x)
-#         x = self.block5_5(x)
-#         x = self.block5_6(x)
-#         x = self.conv6_1(x)
-#         x1 = self.avg_pool1(x)
+#         x3 = self.backbone(x)
+#         x1 = self.avg_pool1(self.x1)
+#         x2 = self.avg_pool2(self.x2)
+        
 #         x1 = x1.view(x1.size(0), -1)
-
-#         x = self.conv7(x)
-#         x2 = self.avg_pool2(x)
 #         x2 = x2.view(x2.size(0), -1)
-
-#         x3 = self.relu(self.conv8(x))
 #         x3 = x3.view(x3.size(0), -1)
-
+        
 #         multi_scale = torch.cat([x1, x2, x3], 1)
 #         landmarks = self.fc(multi_scale)
+#         return self.out1, landmarks
 
-#         return out1, landmarks
+class PFLDInference(nn.Module):
+    def __init__(self):
+        super(PFLDInference, self).__init__()
+
+        self.conv1 = nn.Conv2d(3,
+                               64,
+                               kernel_size=3,
+                               stride=2,
+                               padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(64,
+                               64,
+                               kernel_size=3,
+                               stride=1,
+                               padding=1,
+                               bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv3_1 = InvertedResidual(64, 64, 2, False, 2)
+
+        self.block3_2 = InvertedResidual(64, 64, 1, True, 2)
+        self.block3_3 = InvertedResidual(64, 64, 1, True, 2)
+        self.block3_4 = InvertedResidual(64, 64, 1, True, 2)
+        self.block3_5 = InvertedResidual(64, 64, 1, True, 2)
+
+        self.conv4_1 = InvertedResidual(64, 128, 2, False, 2)
+
+        self.conv5_1 = InvertedResidual(128, 128, 1, False, 4)
+        self.block5_2 = InvertedResidual(128, 128, 1, True, 4)
+        self.block5_3 = InvertedResidual(128, 128, 1, True, 4)
+        self.block5_4 = InvertedResidual(128, 128, 1, True, 4)
+        self.block5_5 = InvertedResidual(128, 128, 1, True, 4)
+        self.block5_6 = InvertedResidual(128, 128, 1, True, 4)
+
+        self.conv6_1 = InvertedResidual(128, 16, 1, False, 2)  # [16, 14, 14]
+
+        self.conv7 = conv_bn(16, 32, 3, 2)  # [32, 7, 7]
+        self.conv8 = nn.Conv2d(32, 128, 7, 1, 0)  # [128, 1, 1]
+        self.bn8 = nn.BatchNorm2d(128)
+
+        self.avg_pool1 = nn.AvgPool2d(14)
+        self.avg_pool2 = nn.AvgPool2d(7)
+        self.fc = nn.Linear(176, 196)
+
+    def forward(self, x):  # x: 3, 112, 112
+        x = self.relu(self.bn1(self.conv1(x)))  # [64, 56, 56]
+        x = self.relu(self.bn2(self.conv2(x)))  # [64, 56, 56]
+        x = self.conv3_1(x)
+        x = self.block3_2(x)
+        x = self.block3_3(x)
+        x = self.block3_4(x)
+        out1 = self.block3_5(x)
+
+        x = self.conv4_1(out1)
+        x = self.conv5_1(x)
+        x = self.block5_2(x)
+        x = self.block5_3(x)
+        x = self.block5_4(x)
+        x = self.block5_5(x)
+        x = self.block5_6(x)
+        x = self.conv6_1(x)
+        x1 = self.avg_pool1(x)
+        x1 = x1.view(x1.size(0), -1)
+
+        x = self.conv7(x)
+        x2 = self.avg_pool2(x)
+        x2 = x2.view(x2.size(0), -1)
+
+        x3 = self.relu(self.conv8(x))
+        x3 = x3.view(x3.size(0), -1)
+
+        multi_scale = torch.cat([x1, x2, x3], 1)
+        landmarks = self.fc(multi_scale)
+
+        return out1, landmarks
 
 
 class AuxiliaryNet(nn.Module):
